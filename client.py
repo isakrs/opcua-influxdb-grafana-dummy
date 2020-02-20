d7f32383b7c74f83690a2c3265f8da3605573fcc
Isak Rathe Stoere <isak.rathe.stoere@yara.com>
2020-02-20 13:55:19 +0100

    Add basic server and client

---
+++ client.py
@@ -0,0 +1,25 @@
+from opcua import Client
+import time
+
+
+url = "opc.tcp://localhost:4840"
+
+client = Client(url)
+
+client.connect()
+print("client connected")
+
+while True:
+	temp = client.get_node("ns=2;i=2")
+	temp_value = temp.get_value()
+
+	press = client.get_node("ns=2;i=3")
+	press_value = press.get_value()
+
+	timestamp = client.get_node("ns=2;i=4")
+	timestamp_value = timestamp.get_value()
+
+	print(temp_value, press_value, timestamp_value)
+
+	time.sleep(1.0)
+
